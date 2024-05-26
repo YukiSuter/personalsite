8c100d8300c71112a9184954b1b82bf1a558d56b
Yuki Suter <yukisuter@ycsuter.dev>
2024-05-26 02:37:16 +0100

    debug prints

--- website/website/settings.py
+++ website/website/settings.py
@@ -137,6 +137,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+print(STATICFILES_DIRS)
+print(BASE_DIR)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
