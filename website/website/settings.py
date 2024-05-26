dbd0146bd999018373881aed00139103e34a7253
Yuki Suter <yukisuter@ycsuter.dev>
2024-05-26 02:16:51 +0100

    collectilfilesdirs

--- website/website/settings.py
+++ website/website/settings.py
@@ -133,6 +133,12 @@ USE_TZ = True
 STATIC_ROOT = '/static/'
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
