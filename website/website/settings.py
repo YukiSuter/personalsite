d88aee99a732ef80e246cb1cd316da02d1a8da7b
Yuki Suter <yukisuter@ycsuter.dev>
2024-06-23 03:17:48 +0100

    i h8 env variables </3

--- website/website/settings.py
+++ website/website/settings.py
@@ -17,6 +17,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+print("ENV: ", os.getenv("DEBUG"))
+
 
 
 if os.getenv("DEBUG") == False:
